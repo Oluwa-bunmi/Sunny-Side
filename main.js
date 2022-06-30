dfce15124ea8f63cffeff46f2bcbee8e887657ad
Jummy007 <gbemisoyeadejumoke@gmail.com>
2022-06-30 03:48:41 +0100

    js working perfectly

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
-let show = document.getElementById("show")
-show.onclick = function(){
-   show.classList.toggle("fa-times");
-
-} 
+let show = document.getElementById("show");
+let close = document.getElementById("hide");
+let nav = document.querySelector(".navLinks");
+show.onclick = function () {
+  show.style.display = "none";
+  close.style.display = "block";
+  nav.style.display = "flex";
+};
+close.onclick = function () {
+  nav.style.display = "none";
+  show.style.display = "block";
+  close.style.display = "none";
+};
