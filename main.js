e7696ecd248abb97917d2fb23b85568b5fe87f62
Jummy007 <gbemisoyeadejumoke@gmail.com>
2022-07-22 16:06:23 +0100

    removed unnecessary comments

--- main.js
+++ main.js
@@ -1,21 +1,12 @@
 let show = document.getElementById("show");
-let close = document.getElementById("hide");
 let nav = document.querySelector(".navLinks");
 show.onclick = function () {
   show.classList.toggle("fa-times");
-  // close.style.display = "block";
-  // nav.style.display = "flex";
-  // nav.style.transition = "10s";
   nav.classList.toggle("responsive");
 };
-// close.onclick = function () {
-//   nav.style.display = "none";
-//   show.style.display = "block";
-//   close.style.display = "none";
-// };
 
 // preloader
 var loader = document.getElementById("preloader");
-window.addEventListener("load", () =>{
+window.addEventListener("load", () => {
   loader.style.display = "none";
 });
