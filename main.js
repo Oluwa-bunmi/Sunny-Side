e60acc3b204399ac6d2bfebe24c530696b052e90
Jummy007 <gbemisoyeadejumoke@gmail.com>
2022-07-22 16:04:44 +0100

    modified js code

--- main.js
+++ main.js
@@ -2,15 +2,17 @@ let show = document.getElementById("show");
 let close = document.getElementById("hide");
 let nav = document.querySelector(".navLinks");
 show.onclick = function () {
-  show.style.display = "none";
-  close.style.display = "block";
-  nav.style.display = "flex";
+  show.classList.toggle("fa-times");
+  // close.style.display = "block";
+  // nav.style.display = "flex";
+  // nav.style.transition = "10s";
+  nav.classList.toggle("responsive");
 };
-close.onclick = function () {
-  nav.style.display = "none";
-  show.style.display = "block";
-  close.style.display = "none";
-};
+// close.onclick = function () {
+//   nav.style.display = "none";
+//   show.style.display = "block";
+//   close.style.display = "none";
+// };
 
 // preloader
 var loader = document.getElementById("preloader");
